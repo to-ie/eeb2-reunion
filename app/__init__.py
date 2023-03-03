7d87706812e750273893b57f11288d050c2d8e82
theodorekall <theodorekall@protonmail.com>
2023-03-03 16:02:15 +0000

    email functionality - tester with sending error messages to admin

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,3 +16,33 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+if not app.debug:
+    # Log sent to email
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='eeb2@t-o.ie',
+            # fromaddr='eeb2@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='ERROR WARNING - EEB2 Reunion webapp',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # Log to file
+    if not os.path.exists('logs'):
+            os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/eeb2.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('eeb2 Reunion')
--- config.py
+++ config.py
@@ -11,3 +11,17 @@ class Config(object):
     MAX_CONTENT_LENGTH = 2048 * 2048
     UPLOAD_PATH = 'static/uploads/'
 
+    MAIL_SERVER = 'smtp.hostinger.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 0
+    MAIL_USERNAME = 'eeb2@t-o.ie'
+    MAIL_PASSWORD = 'Thisisthepassword1!'
+    # TODO: Change password
+    ADMINS = ['eeb2@t-o.ie']
+
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ADMINS = ['your-email@example.com']
