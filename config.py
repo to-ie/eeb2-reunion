2bac3558ef66bb5bc08301ed1fe3c3962d55c6f1
theodorekall <theodorekall@protonmail.com>
2023-03-03 16:05:37 +0000

    gitignore test

--- config.py
+++ config.py
@@ -18,10 +18,3 @@ class Config(object):
     MAIL_PASSWORD = 'Thisisthepassword1!'
     # TODO: Change password
     ADMINS = ['eeb2@t-o.ie']
-
-    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # ADMINS = ['your-email@example.com']
